df881cb3a0cce932272e0af622e089d5efd40b1f
Tejas8075 <mahangadetejas09@gmail.com>
2024-02-28 20:20:09 +0530

    Add new project

---
+++ Insta love button/script.js
@@ -0,0 +1,16 @@
+let con = document.querySelector("#container");
+let icon = document.querySelector("i");
+
+con.addEventListener("dblclick", ()=>{
+    icon.style.transform = "translate(-50%, -50%) scale(1)";
+    icon.style.color = "red"
+    icon.style.opacity = 0.8;
+
+    setTimeout(()=>{
+        icon.style.opacity = 0;
+    },1000)
+    
+    setTimeout(()=>{
+        icon.style.transform = "translate(-50%, -50%) scale(0)";
+    },2000)
+})
