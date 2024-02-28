7e6b926eaabd26254a57e386c6efa2cb8ce3c06a
Tejas8075 <mahangadetejas09@gmail.com>
2024-02-28 21:52:54 +0530

    Add half part

---
+++ New mouse cursor/script.js
@@ -0,0 +1,18 @@
+let elem1 = document.querySelector(".elem1");
+
+
+// let elemImg = document.querySelector("#elem1 img");
+
+// elem1.addEventListener("mousemove", (dets)=>{
+//     elemImg.style.left = dets.x + "px";
+//     elemImg.style.top = dets.y + "px";
+    
+// })
+
+// elem1.addEventListener("mouseenter", (dets)=>{
+//     elemImg.style.opacity = 1;
+// })
+
+// elem1.addEventListener("mouseleave", (dets)=>{
+//     elemImg.style.opacity = 0;
+// })
